ba62a592aa698cd66204d143bdf640e38b3131e6
Athunder79 <athunder@me.com>
2024-03-05 22:42:49 +0000

    test

--- birdie_iview/core/views.py
+++ birdie_iview/core/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from django.views.generic import CreateView, ListView
 from django.conf import settings
 from .forms import ShotForm
-from .models import Shot
+from .models import Shot, Course
+import googlemaps
+from django.shortcuts import render
 
 
 # Create your views here.
@@ -24,6 +26,34 @@ def scorecard(request):
     else:
         form = ShotForm()
         return render(request, 'core/scorecard.html', {'form': form})
+
+def find_golf_courses(request):
+    gmaps = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
+    location = (53.3498053, -6.2603097)  # Dublin coordinates
+    radius = 10000  # 10km radius
+
+    # Search for golf courses nearby
+    places = gmaps.places_nearby(location, radius=radius, type='golf_course')
+
+    # Process the results and save to Course model
+    golf_courses = []
+    if 'results' in places:
+        for place in places['results']:
+            if 'golf_course' in place.get('types', []):
+                course_info = {
+                    'name': place['name'],
+                    'address': place['vicinity'],
+                    'rating': place.get('rating', None),
+                   
+                    'longitude': place['geometry']['location']['lng'],
+                }
+                # Check for duplicates based on coordinates
+                if not Course.objects.filter(latitude=course_info['latitude'], longitude=course_info['longitude']).exists():
+                    golf_courses.append(course_info)
+                    # Create and save Course object
+                    Course.objects.create(**course_info)
+    return render(request, 'core/find_golf_courses.html', {'golf_courses': golf_courses})
+
 
 class CoreListView(ListView):
     template_name = 'core/home.html'
