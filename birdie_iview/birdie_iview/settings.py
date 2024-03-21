a18621d1902cf4e2d0f39e10a3a86d9a8de9d6c4
Athunder79 <athunder@me.com>
2024-03-21 19:12:41 +0000

    dj_database_url - Postgres

--- birdie_iview/birdie_iview/settings.py
+++ birdie_iview/birdie_iview/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
+import dj_database_url
 
 
 
@@ -81,13 +81,8 @@ WSGI_APPLICATION = 'birdie_iview.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# database url is set in the environment variable DATABASE_URL
+DATABASES = {"default": dj_database_url.config(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
