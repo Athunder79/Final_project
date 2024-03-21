af4edb7c2a7b15195e57997fad8a68f5aa84b393
Athunder79 <athunder@me.com>
2024-03-21 20:27:44 +0000

    set secret key and debug to  use enviroment variables

--- birdie_iview/birdie_iview/settings.py
+++ birdie_iview/birdie_iview/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tvi#v*3^=b*v)9uaqn-txlfgzo33zz7l4+rv83i+x+pvxj1s*#'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'TRUE'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
 
